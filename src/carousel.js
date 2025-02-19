3351426e382e05178e490d8f8700b623e8639ca4
Charles Deane <30459123+cbdeane@users.noreply.github.com>
2025-02-18 20:47:04 -0500

    new carousel nav

--- src/carousel.js
+++ src/carousel.js
@@ -5,6 +5,7 @@ const section = document.getElementById('hero_carousel');
 const hero_content = document.getElementById('hero_content'); //the div that will hold the html content
 const left_arrow = document.getElementById('carousel_leftarrow_div');
 const right_arrow = document.getElementById('carousel_rightarrow_div');
+const placekeeper_div = document.getElementById('carousel_placekeeper_div');
 
 // Current index of the displayed item
 let currentIndex = 0;
@@ -21,7 +22,7 @@ function loadCarouselItem(index) {
       hero_content.innerHTML = html;
     })
     .catch(error => console.error('Error loading HTML:', error));
-  currentSlide = document.querySelector('.carousel_module');
+  animateBubbles();
 }
 
 // Show the next carousel item
@@ -63,7 +64,34 @@ function startAutoSlide() {
   autoSlideInterval = setInterval(showNextItem, SLIDE_INTERVAL);
 }
 
+function populateBubbles() {
+  for (let i = 0; i < DIRECTORY.length; i++) {
+    const bubble = document.createElement('div');
+    bubble.classList.add('bubble');
+    bubble.addEventListener('click', () => {
+      currentIndex = i;
+      loadCarouselItem(currentIndex);
+      pauseAutoSlide(); // Pause when manually navigated
+      startAutoSlide();
+    });
+    placekeeper_div.appendChild(bubble);
+  }
+}
+
+function animateBubbles() {
+  const bubbles = document.querySelectorAll('.bubble');
+  bubbles.forEach((bubble, i) => {
+    if (i === currentIndex) {
+      bubble.classList.add('bubble_selected');
+    } else {
+      bubble.classList.remove('bubble_selected');
+    }
+  });
+}
 // Initialize carousel by loading the first item and starting the auto slide
+
+populateBubbles();
+animateBubbles();
 loadCarouselItem(currentIndex);
 startAutoSlide();
 
