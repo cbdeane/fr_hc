5943c9bccdc4b340f266ff2bc36decf2982b2431
Charles Deane <30459123+cbdeane@users.noreply.github.com>
2025-02-21 16:18:57 -0500

    I never want to do another accordion ever again, but its done

--- src/accordion_question.js
+++ src/accordion_question.js
@@ -1,27 +1,37 @@
-const objectArray = Array.from(document.getElementsByClassName("section_questions"));
-const questionArray = Array.from(document.getElementsByClassName("content_questions"));
+const questionArray = Array.from(document.getElementsByClassName("question_wrapper"));
 const addboxArray = Array.from(document.getElementsByClassName("addbox"));
 const minusboxArray = Array.from(document.getElementsByClassName("minusbox"));
 const answerArray = Array.from(document.getElementsByClassName("content_answer"));
+const measureArray = [];
+
+
+//quickly measure the answers then hide them
+for (let i = 0; i < answerArray.length; i++) {
+  measureArray.push(answerArray[i].scrollHeight);
+  answerArray[i].style.height = "0px";
+  answerArray[i].classList.add("content_answer_hidden");
+}
+
+
 
 // add event listener to each element in the questionArray
 for (let i = 0; i < questionArray.length; i++) {
   questionArray[i].addEventListener("click", () => {
-    if (questionArray[i].classList.contains("content_questions_closed")) {
-      questionArray[i].classList.remove("content_questions_closed");
-      questionArray[i].classList.add("content_questions_opened");
+    if (questionArray[i].classList.contains("question_wrapper_off")) {
+      questionArray[i].classList.remove("question_wrapper_off");
+      questionArray[i].classList.add("question_wrapper_on");
     } else {
-      questionArray[i].classList.remove("content_questions_opened");
-      questionArray[i].classList.add("content_questions_closed");
+      questionArray[i].classList.remove("question_wrapper_on");
+      questionArray[i].classList.add("question_wrapper_off");
     }
-
-    if (answerArray[i].classList.contains("content_answer")) {
-      answerArray[i].classList.remove("content_answer");
+    if (!answerArray[i].classList.contains("content_answer_hidden")) {
       answerArray[i].classList.add("content_answer_hidden");
+      answerArray[i].style.height = "0px";
     } else {
       answerArray[i].classList.remove("content_answer_hidden");
-      answerArray[i].classList.add("content_answer");
+      answerArray[i].style.height = measureArray[i] + "px";
     }
+
 
     if (addboxArray[i].classList.contains("question_box_hidden")) {
       addboxArray[i].classList.remove("question_box_hidden");
